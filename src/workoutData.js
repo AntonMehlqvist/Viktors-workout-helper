994f4d26086660352415733f3d88cfd67542b233
Anton Mehlqvist <anton.mehlqvist@angrycreative.se>
2020-12-19 18:33:07 +0100

    data added

---
+++ src/workoutData.js
@@ -0,0 +1,73 @@
+const data = [
+	{
+		title: 'Helkroppspass med motståndsband',
+		image: 'https://cdn2.coachmag.co.uk/sites/coachmag/files/styles/16x9_760/public/2017/08/lets-bands-resistance-bands.jpg?itok=skCYawy5&timestamp=1503481629',
+		exercises: [
+			{
+				title: 'One-arm biceps curl',
+				image: 'https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-one-arm-biceps-curls.jpg?itok=UDvnqQAo',
+				sets: '1',
+				reps: '12-15 on each side',
+				description: 'Stand with feet shoulder-width apart with both feet on the resistance band. Grasp the band with one hand and hold it with your arm down at your side, palm facing forwards. Bend at the elbow and lift your arm toward your shoulders until you get a good biceps contraction. Then lower slowly back to the start. Do all your reps on one arm, then switch.'
+			},
+			{
+				title: 'Flye',
+				image: 'https://cdn2.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-flye.jpg?itok=OBXcibAy',
+				sets: '1',
+				reps: '12-15',
+				description: 'Hold the resistance band in both hands, arms stretched straight out to the sides at chest height, with the band going behind your back. Press the band straight out in front of you, bringing your hands together with your arms fully extended, keeping your elbows up throughout and squeezing your chest muscles as you press. Slowly return to the starting position.'
+			},
+			{
+				title: 'Front squat',
+				image: 'https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-front-squat.jpg?itok=iQJZrj6n',
+				sets: '1',
+				reps: '8-15',
+				description: 'Stand on the resistance band with your feet slightly wider than shoulder-width apart, and bring the top of the band up to rest on the front of your shoulders. Lower into a squat, with your chest up and your knees over your toes. Then push up to the starting position.'
+			},
+			{
+				title: 'Side-lying hip abduction',
+				image: 'https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-hip-abduction-.jpg?itok=qGbpHxy3',
+				sets: '1',
+				reps: '10-12 each side',
+				description: 'Lie on your side with your hips and knees bent at 90° and the resistance band looped just above your knees. Raise the upper leg to pull your knees apart while contracting your glutes for two to three seconds, then slowly return to the starting position. Do all your reps on one side, then switch.'
+			},
+			{
+				title: 'Glute bridge',
+				image: 'https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-glute-bridge.jpg?itok=Wl2P9bKw',
+				sets: '1',
+				reps: '10-15',
+				description: 'Loop the resistance band just above your knees and lie on your back with your feet on the floor and your knees bent at 90°. Lift your toes off the floor, then raise your hips until you form a straight line from your knees to your shoulders, contracting your glutes throughout the entire movement. As you raise your hips open your knees slightly to press against the resistance band.'
+			},
+			{
+				title: 'Splitter',
+				image: 'https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-splitter.jpg?itok=NzJ-iIz1',
+				sets: '1',
+				reps: '8-10',
+				description: 'Stand with your knees slightly bent and your feet shoulder-width apart. Grip a shoulder-width section of the resistance band with both hands in front of you at shoulder height. Keeping your arms straight, pull the band out and back until your shoulder blades contract. Slowly return to starting position.'
+			},
+			{
+				title: 'Lateral walk',
+				image: 'https://cdn1.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-lateral-walk.jpg?itok=jFD10yki',
+				sets: '1',
+				reps: '8-10 steps in each direction',
+				description: 'Loop one resistance band just above your knees and another around your ankles. Drop into a half squat position with your feet shoulder-width apart to create tension in the bands. Then take a small step to the side, keeping tension in the bands as you move. Do all the steps in one direction, then switch.'
+			},
+			{
+				title: 'Press-up',
+				image: 'https://cdn2.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-push-up.jpg?itok=AU1e9X1m',
+				sets: '1',
+				reps: '5-15',
+				description: 'Get in a plank position, draping the resistance band across your upper back and holding the ends under your hands. Drop your chest down towards the floor, then contract your glutes and abs and push straight up until your arms are fully extended'
+			},
+			{
+				title: 'Squat to overhead press',
+				image: 'https://cdn2.coachmag.co.uk/sites/coachmag/files/styles/insert_main_wide_image/public/2017/08/resistance-band-squat-to-overhead-press.jpg?itok=tmpjlMiN',
+				sets: '1',
+				reps: '8-10',
+				description: 'Stand on the resistance band with your feet shoulder-width apart. Position your hands at shoulder level with your palms facing up, holding the resistance band. Drop into a squat, then push back up and fully extend your arms so you raise the resistance band overheard. Then lower slowly into another squat.'
+			},
+		]
+	}
+];
+
+export default data;
