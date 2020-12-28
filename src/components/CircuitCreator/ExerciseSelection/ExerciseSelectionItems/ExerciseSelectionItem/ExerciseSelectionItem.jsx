132e9f0ef70fa525ab8e70e14d8fca4410813b98
Anton Mehlqvist <anton.mehlqvist@angrycreative.se>
2020-12-28 20:00:14 +0100

    Starting styling

--- src/components/CircuitCreator/ExerciseSelection/ExerciseSelectionItems/ExerciseSelectionItem/ExerciseSelectionItem.jsx
+++ src/components/CircuitCreator/ExerciseSelection/ExerciseSelectionItems/ExerciseSelectionItem/ExerciseSelectionItem.jsx
@@ -7,15 +7,22 @@ const ExerciseSelectionItem = ({
 } ) => {
 	const [ isSelected, setIsSelected ] = useState( false );
   return (
-    <div>
-      <div className="flex justify-between py-4 border-b-2 border-gray-400">
+    <div className="py-4 border-b-2 border-gray-400">
+      <div className="flex justify-between">
         <h4>{title}</h4>
 
-        <button
-				className="w-4 mr-2"
-				onClick={ () => setIsSelected( ! isSelected ) }>
-					{isSelected ? <Minus /> : <Plus />}
-				</button>
+        <button className="w-4 mr-2" onClick={() => setIsSelected(!isSelected)}>
+          {isSelected ? <Minus /> : <Plus />}
+        </button>
+      </div>
+
+      <div className={ `${ isSelected ? 'h-max' : 'h-0' } overflow-hidden` }>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        dolores molestiae aliquid obcaecati neque sit, debitis omnis soluta ab
+        hic facilis quibusdam consectetur voluptas vero? Provident, laudantium
+        esse? Dolore iure dolores necessitatibus nesciunt illum odio blanditiis
+        quas voluptates. Expedita ratione illum magnam cumque doloremque nobis
+        ipsum iure veritatis, quasi doloribus?
       </div>
     </div>
   );
