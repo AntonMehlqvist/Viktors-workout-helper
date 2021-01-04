823bd92cafdbeeaf4d9751b85dce963d598e6ec1
Anton Mehlqvist <anton.mehlqvist@angrycreative.se>
2021-01-04 21:04:24 +0100

    options added

---
+++ src/components/CircuitCreator/ExerciseSelection/ExerciseSelectionItems/ExerciseSelectionItem/ExerciseOptions/ExerciseOptions.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import ExerciseOption from './ExerciseOption';
+
+const ExerciseOptions = ( { exercise: { minReps, maxReps, time: exerciseTime } } ) => {
+	const [ reps, setReps ] = useState( maxReps ?? minReps );
+	const [ time, setTime ] = useState( exerciseTime );
+
+	const setOptionsHandler = ( currentValue, value, callback ) => {
+		if ( currentValue !== 0 ) {
+			callback( value );
+		}
+	}
+
+	return (
+    <ul className="py-4 list-none">
+      { time && (
+        <ExerciseOption
+					name="Time"
+          currentValue={ time }
+          setOption={ ( newValue ) => setOptionsHandler( time, newValue, setTime ) }
+        />
+      ) }
+
+      { reps && (
+        <ExerciseOption
+					name="Reps"
+          currentValue={ reps }
+          setOption={ ( newValue ) => setOptionsHandler( reps, newValue, setReps ) }
+        />
+      ) }
+    </ul>
+  );
+}
+
+export default ExerciseOptions
